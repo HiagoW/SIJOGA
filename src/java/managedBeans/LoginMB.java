fe5ab241b4869a12b5a6a6eec6d701a90e9bb345
Hiago William Petris <hiago.petris@gmail.com>
2020-06-09 15:55:57 -0300

    Criação do MB processos

--- src/java/managedBeans/LoginMB.java
+++ src/java/managedBeans/LoginMB.java
@@ -33,12 +33,6 @@ public class LoginMB implements Serializable {
     private String email;
     private String senha;
     private Usuario usuario;
-    private List<Processo> processos = new ArrayList<>();
-    private List<Processo> processosPromovente = new ArrayList<>();
-    private List<Processo> processosPromovido = new ArrayList<>();
-    private long processosAtivos = 0;
-    private long processosAguardandoIntimacao = 0;
-    private long processosEncerrados = 0;
     
     public LoginMB() {
     }
@@ -66,38 +60,6 @@ public class LoginMB implements Serializable {
         this.senha = senha;
     }
 
-    public List<Processo> getProcessos() {
-        return processos;
-    }
-
-    public void setProcessos(List<Processo> processos) {
-        this.processos = processos;
-    }
-    
-    public List<Processo> getProcessosPromovente() {
-        return processosPromovente;
-    }
-
-    public void setProcessosPromovente(List<Processo> processos) {
-        this.processosPromovente = processos;
-    }
-    
-    public List<Processo> getProcessosPromovido() {
-        return processosPromovido;
-    }
-
-    public void setProcessosPromovido(List<Processo> processos) {
-        this.processosPromovido = processos;
-    }
-
-    public long getProcessosAtivos() {
-        return processosAtivos;
-    }
-
-    public void setProcessosAtivos(long processosAtivos) {
-        this.processosAtivos = processosAtivos;
-    }
-
     public Usuario getUsuario() {
         return usuario;
     }
@@ -105,24 +67,6 @@ public class LoginMB implements Serializable {
     public void setUsuario(Usuario usuario) {
         this.usuario = usuario;
     }
-
-    public long getProcessosAguardandoIntimacao() {
-        return processosAguardandoIntimacao;
-    }
-
-    public void setProcessosAguardandoIntimacao(long processosAguardandoIntimacao) {
-        this.processosAguardandoIntimacao = processosAguardandoIntimacao;
-    }
-
-    public long getProcessosEncerrados() {
-        return processosEncerrados;
-    }
-
-    public void setProcessosEncerrados(long processosEncerrados) {
-        this.processosEncerrados = processosEncerrados;
-    }
-    
-    
     
     public String realizaLogin(){
         try{
@@ -135,22 +79,9 @@ public class LoginMB implements Serializable {
             if(usuario!=null){
                 switch(usuario.getTipo().getDescricao()){
                     case "Juiz":
-                        processos = ProcessoFacade.buscaProcessos(usuario);
-                        for(Processo p : processos){
-                            if(p.getFaseAtual().getFase().getId() == 6){
-                                processosEncerrados+=1;
-                            }else if(p.getFaseAtual().getFase().getId() == 5){
-                                processosAguardandoIntimacao+=1;
-                            }else{
-                                processosAtivos+=1;
-                            }
-                        }
                         SessionContext.getInstance().setAttribute("usuarioLogado", usuario);
                         return "/juiz/home.xhtml?faces-redirect=true";
                     case "Advogado":
-                        processosPromovente = ProcessoFacade.buscaProcessosAdvPromovente(usuario);
-                        processosPromovido = ProcessoFacade.buscaProcessosAdvPromovido(usuario);
-                        /*processosAtivos = ProcessoFacade.processosAtivos(usuario);*/
                         SessionContext.getInstance().setAttribute("usuarioLogado", usuario);
                         return "/advogado/home.xhtml?faces-redirect=true";
                     default:
---
+++ src/java/managedBeans/ProcessosMB.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managedBeans;
+
+import beans.Processo;
+import beans.Usuario;
+import facade.LoginFacade;
+import facade.ProcessoFacade;
+import java.io.IOException;
+import java.io.Serializable;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import util.SessionContext;
+
+/**
+ *
+ * @author hiago
+ */
+@Named
+@RequestScoped
+public class ProcessosMB implements Serializable {
+    
+    private List<Processo> processos = new ArrayList<>();
+    private List<Processo> processosPromovente = new ArrayList<>();
+    private List<Processo> processosPromovido = new ArrayList<>();
+    private long processosAtivos = 0;
+    private long processosAguardandoIntimacao = 0;
+    private long processosEncerrados = 0;
+    
+    public ProcessosMB() {
+    }
+    
+    @PostConstruct
+    public void init(){
+        Usuario usuario = (Usuario) SessionContext.getInstance().getAttribute("usuarioLogado");
+        if(usuario.getTipo().getId()==1){
+            processos = ProcessoFacade.buscaProcessos(usuario);
+            for(Processo p : processos){
+                if(p.getFaseAtual().getFase().getId() == 6){
+                    processosEncerrados+=1;
+                }else if(p.getFaseAtual().getFase().getId() == 5){
+                    processosAguardandoIntimacao+=1;
+                }else{
+                    processosAtivos+=1;
+                }
+            }
+        }else if(usuario.getTipo().getId()==2){
+            processosPromovente = ProcessoFacade.buscaProcessosAdvPromovente(usuario);
+            processosPromovido = ProcessoFacade.buscaProcessosAdvPromovido(usuario);
+            /*processosAtivos = ProcessoFacade.processosAtivos(usuario);*/
+        }
+    }
+    
+    public List<Processo> getProcessos() {
+        return processos;
+    }
+
+    public void setProcessos(List<Processo> processos) {
+        this.processos = processos;
+    }
+    
+    public List<Processo> getProcessosPromovente() {
+        return processosPromovente;
+    }
+
+    public void setProcessosPromovente(List<Processo> processos) {
+        this.processosPromovente = processos;
+    }
+    
+    public List<Processo> getProcessosPromovido() {
+        return processosPromovido;
+    }
+
+    public void setProcessosPromovido(List<Processo> processos) {
+        this.processosPromovido = processos;
+    }
+
+    public long getProcessosAtivos() {
+        return processosAtivos;
+    }
+
+    public void setProcessosAtivos(long processosAtivos) {
+        this.processosAtivos = processosAtivos;
+    }
+
+
+    public long getProcessosAguardandoIntimacao() {
+        return processosAguardandoIntimacao;
+    }
+
+    public void setProcessosAguardandoIntimacao(long processosAguardandoIntimacao) {
+        this.processosAguardandoIntimacao = processosAguardandoIntimacao;
+    }
+
+    public long getProcessosEncerrados() {
+        return processosEncerrados;
+    }
+
+    public void setProcessosEncerrados(long processosEncerrados) {
+        this.processosEncerrados = processosEncerrados;
+    }
+}
